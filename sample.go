bcc163bf6607ac29e9775ad8106246281625a43f
kenzo <kenzou.kenzou104809@gmail.com>
2020-09-22 19:52:10 +0900

    feat: 画像の変換処理をするコード追加

--- sample.go
+++ sample.go
@@ -3,22 +3,51 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
-	"io/ioutil"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"os"
+	"strings"
 )
 
-
 // @see https://news.mynavi.jp/article/gogogo-5/
+// @see https://shiro-16.hatenablog.com/entry/2020/05/29/130508
 func main() {
-	files, err := ioutil.ReadDir(".")
+	flag.Parse()
+	args := flag.Args()
+
+	f, err := os.Open(args[0]) // 元画像読み込み
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("open:", err)
+		return
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f) // 元画像デコード
+	if err != nil {
+		fmt.Println("decode:", err)
+		return
 	}
 
-	for _, file := range files {
-		fmt.Println(file.Name())
+	fso, err := os.Create(args[1]) // 変換後画像作成
+	if err != nil {
+		fmt.Println("create:", err)
+		return
+	}
+	defer fso.Close()
+
+	slice := strings.Split(args[1], ".")
+
+	switch slice[len(slice)-1] { // 出力画像の拡張子によってエンコードを変える
+	case "jpeg", "jpg":
+		jpeg.Encode(fso, img, &jpeg.Options{})
+	case "png":
+		png.Encode(fso, img)
+	case "gif":
+		gif.Encode(fso, img, nil)
+	default:
 	}
 }
-
-// @see https://shiro-16.hatenablog.com/entry/2020/05/29/130508
