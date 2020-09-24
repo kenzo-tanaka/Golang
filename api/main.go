32b9e9fb2d28954c31ff27b546e21f6ff39cd4d4
kenzo <kenzou.kenzou104809@gmail.com>
2020-09-24 19:45:56 +0900

    API の実装を標準ライブラリだけで実装

---
+++ api/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "welcome")
+	fmt.Println("Endpoint Hit: Homepage")
+}
+
+// http://localhost:8081
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", returnAllArticles)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{}
+
+	for i := 0; i < 10; i++ {
+		title := "Hello_%d"
+		articles = append(
+			articles,
+			Article{Title: fmt.Sprintf(title, i), Desc: "Article Desc", Content: "Article Content"})
+	}
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(articles)
+
+}
+
+func main() {
+	handleRequests()
+}
